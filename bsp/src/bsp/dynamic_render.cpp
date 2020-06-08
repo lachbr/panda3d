8df11292c11943cef9c9452062cd5dd89ac3e4c2
Brian Lach <brianlach72@gmail.com>
2020-06-08 00:09:31 -0400

    bsp: dynamic geometry rendering system

---
+++ bsp/src/bsp/dynamic_render.cpp
@@ -0,0 +1,180 @@
+#include "dynamic_render.h"
+#include "geomTriangles.h"
+#include "geomLines.h"
+#include "geomLinestrips.h"
+#include "clockObject.h"
+#include "geomVertexWriter.h"
+#include "internalName.h"
+
+NotifyCategoryDeclNoExport(dynamicrender)
+NotifyCategoryDef(dynamicrender, "")
+
+DynamicRender::
+DynamicRender() {
+  _geom = nullptr;
+  _drawing = false;
+  _modified = true;
+  _gn = new GeomNode("dynamicGeometry");
+  _np = NodePath(_gn);
+}
+
+void DynamicRender::
+set_draw_mask(const BitMask32 &mask) {
+  _np.hide(BitMask32::all_on());
+  _np.show_through(mask);
+}
+
+void DynamicRender::
+render_state(const RenderState *rs) {
+  nassertv(!_drawing);
+  _context.state = rs;
+}
+
+void DynamicRender::
+primitive_type(DynamicRender::PrimitiveType type) {
+  nassertv(!_drawing);
+  _context.prim_type = type;
+}
+
+void DynamicRender::
+vertex_format(const GeomVertexFormat *format) {
+  nassertv(!_drawing);
+  _context.format = format;
+}
+
+void DynamicRender::
+begin() {
+  nassertv(!_drawing);
+
+  _modified = true;
+
+  _context.calc_hash();
+  _geom = find_or_create_geom_entry();
+
+  _drawing = true;
+}
+
+void DynamicRender::
+draw() {
+  nassertv(_drawing);
+  _gn->add_geom(_geom->geom, _geom->state);
+  _drawing = false;
+}
+
+DynamicRender::DynamicGeomEntry *DynamicRender::
+find_or_create_geom_entry() {
+  auto itr = _geoms.find(_context.hash);
+  if (itr != _geoms.end()) {
+    return itr->second;
+  }
+
+  // Haven't encountered this render context yet
+  PT(DynamicGeomEntry) entry = new DynamicGeomEntry;
+  entry->state = _context.state;
+  entry->indices = create_indices();
+  entry->vertices = find_or_create_vertex_buffer();
+  entry->geom = new Geom(entry->vertices->get_vdata());
+  entry->geom->add_primitive(entry->indices);
+  _geoms[_context.hash] = entry;
+  return entry;
+}
+
+DynamicVertexBuffer *DynamicRender::
+find_or_create_vertex_buffer(int max_vertices) {
+  auto itr = _vertex_buffers.find(_context.format);
+  if (itr != _vertex_buffers.end()) {
+    return itr->second;
+  }
+
+  PT(DynamicVertexBuffer) buffer = new DynamicVertexBuffer(_context.format, max_vertices);
+  _vertex_buffers[_context.format] = buffer;
+  return buffer;
+}
+
+PT(GeomPrimitive) DynamicRender::
+create_indices(int max_indices) {
+  PT(GeomPrimitive) prim;
+  switch (_context.prim_type) {
+  case PT_triangles:
+    prim = new GeomTriangles(GeomEnums::UH_dynamic);
+    break;
+  case PT_lines:
+    prim = new GeomLines(GeomEnums::UH_dynamic);
+    break;
+  case PT_line_strip:
+    prim = new GeomLinestrips(GeomEnums::UH_dynamic);
+    break;
+  default:
+    prim = nullptr;
+  }
+
+  if (prim) {
+    prim->reserve_num_vertices(max_indices);
+  }
+
+  return prim;
+}
+
+void DynamicRender::
+reset() {
+  if (dynamicrender_cat.is_debug())  {
+    dynamicrender_cat.debug()
+      << "We have " << _geoms.size() << " geoms, " << _vertex_buffers.size() << " vertex buffers\nVertex Buffers:";
+    for (auto itr = _vertex_buffers.begin(); itr != _vertex_buffers.end(); itr++) {
+      std::cout << "----------------------------------" << std::endl;
+      std::cout << "Position: " << itr->second->get_position() << std::endl;
+      std::cout << "Max verts: " << itr->second->get_max_vertices() << std::endl;
+      itr->second->get_vdata()->output(std::cout);
+    }
+    dynamicrender_cat.debug()
+      << "Geoms:\n";
+    for (auto itr = _geoms.begin(); itr != _geoms.end(); itr++) {
+      std::cout << "----------------------------------" << std::endl;
+      itr->second->indices->output(std::cout);
+      itr->second->state->output(std::cout);
+    }
+  }
+  
+  if (_modified) {
+    if (dynamicrender_cat.is_debug()) {
+      dynamicrender_cat.debug()
+        << "Resetting... had " << _gn->get_num_geoms() << " geoms\n";
+    }
+    _gn->remove_all_geoms();
+    for (auto itr = _vertex_buffers.begin(); itr != _vertex_buffers.end(); itr++) {
+      itr->second->reset();
+    }
+    for (auto itr = _geoms.begin(); itr != _geoms.end(); itr++) {
+      itr->second->indices->clear_vertices();
+    }
+
+    _modified = false;
+  }
+}
+
+// Functions for drawing primitives
+
+void DynamicRender::
+draw_rect(const LVector3 &mins, const LVector3 &maxs) {
+  primitive_type(PT_line_strip);
+  vertex_format(GeomVertexFormat::get_v3c4());
+
+  begin();
+
+  int first_index;
+  _geom->vertices->lock(4, first_index);
+
+  GeomVertexWriter vwriter = _geom->vertices->get_writer("vertex");
+  vwriter.set_data3f(mins[0], mins[1], 0);
+  vwriter.set_data3f(mins[0], maxs[1], 0);
+  vwriter.set_data3f(maxs[0], maxs[1], 0);
+  vwriter.set_data3f(maxs[0], mins[1], 0);
+
+  _geom->vertices->unlock();
+
+  _geom->indices->add_consecutive_vertices(first_index, 4);
+  _geom->indices->add_vertex(first_index);
+  _geom->indices->close_primitive();
+
+  draw();
+}
---
+++ bsp/src/bsp/dynamic_render.h
@@ -0,0 +1,106 @@
+#pragma once
+
+/*
+One GeomNode with a specific draw mask
+
+Can have multiple Geoms.
+
+Each Geom has a unique:
+- RenderState
+- Primtive type
+
+One dynamic vertex buffer for each format.
+*/
+
+#include "config_bsp.h"
+#include "geomNode.h"
+#include "nodePath.h"
+#include <unordered_map>
+#include "dynamic_vertex_buffer.h"
+#include "geomEnums.h"
+#include "typeHandle.h"
+#include "geomPrimitive.h"
+#include "referenceCount.h"
+
+class EXPCL_PANDABSP DynamicRender {
+PUBLISHED:
+  enum PrimitiveType {
+    PT_triangles,
+    PT_lines,
+    PT_line_strip
+  };
+
+  class DynamicGeomEntry : public ReferenceCount {
+  public:
+    const RenderState *state;  // shared
+    PT(GeomPrimitive) indices; // not shared
+    DynamicVertexBuffer *vertices; // shared
+    PT(Geom) geom;
+    bool was_reset;
+  };
+
+  class DynamicRenderContext {
+  public:
+    DynamicRenderContext() {
+      state = RenderState::make_empty();
+      format = GeomVertexFormat::get_v3();
+      prim_type = PrimitiveType::PT_triangles;
+      calc_hash();
+    }
+    const RenderState *state;
+    DynamicRender::PrimitiveType prim_type;
+    const GeomVertexFormat *format;
+    size_t hash;
+
+    void calc_hash() {
+      hash = 0u;
+      hash = pointer_hash::add_hash(hash, state);
+      hash = pointer_hash::add_hash(hash, format);
+      hash = int_hash::add_hash(hash, prim_type);
+    }
+  };
+
+  DynamicRender();
+
+  void render_state(const RenderState *rs);
+  void primitive_type(PrimitiveType type);
+  void vertex_format(const GeomVertexFormat *format);
+
+  void draw_rect(const LVector3 &mins, const LVector3 &maxs);
+
+  void begin();
+  void draw();
+
+  void set_draw_mask(const BitMask32 &mask);
+
+  NodePath get_dynamic_render_node_path() const;
+
+  void reset();
+
+private:
+  DynamicGeomEntry *find_or_create_geom_entry();
+  DynamicVertexBuffer *find_or_create_vertex_buffer(int max_vertices = 500);
+  PT(GeomPrimitive) create_indices(int max_indices = 500);
+
+private:
+  PT(GeomNode) _gn;
+  NodePath _np;
+
+  bool _drawing;
+  bool _modified;
+
+  // Current render context
+  DynamicRenderContext _context;
+
+  // Current geom we are working on
+  DynamicGeomEntry *_geom;
+
+  std::unordered_map<size_t, PT(DynamicGeomEntry)> _geoms;
+
+  std::unordered_map<const GeomVertexFormat *, PT(DynamicVertexBuffer)> _vertex_buffers;
+};
+
+INLINE NodePath DynamicRender::
+get_dynamic_render_node_path() const {
+  return _np;
+}
